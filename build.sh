b231780ca473dd26bdcd4997c7caf800d665ad59
Maximilian Schellhorn <mxschell@amazon.de>
2022-11-18 16:26:09 +0100

    Adapted naming

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ if [ -f "$file" ] ; then
     rm "$file"
 fi
 
-# Build the custom Java runtime from the Dockerfile
-docker build -f Dockerfile --progress=plain -t lambda-custom-runtime-minimal-jre-17-x86 .
+# Build the function locally to get the JAR
+mvn clean package -f unicorn-store-spring/pom.xml
+
+# Build a custom Java 17 runtime with jlink from a Dockerfile
+docker build -f JRE17LambdaBuilder.dockerfile --progress=plain -t lambda-custom-runtime-minimal-jre-17-x86 .
 
 # Extract the runtime.zip from the Docker environment and store it locally
 docker run --rm --entrypoint cat lambda-custom-runtime-minimal-jre-17-x86 spring-custom-runtime.zip > spring-custom-runtime.zip
